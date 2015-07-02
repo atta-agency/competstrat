11425fff81a411713ddbbe652e031f0984ed532e
koch <guillermo.jeanphilippe@gmail.com>
2015-07-02 23:15:47 +0200

    Integ landing

--- wp-content/themes/underscore_sass_v1/landing-page.php
+++ wp-content/themes/underscore_sass_v1/landing-page.php
@@ -27,10 +27,42 @@ get_header(); ?>
 				<h1>Krill - Tonic </h1>
 				<p>Préparez-vous à libérer la quintessence des eaux glacées et cyclopéennes du grand nord...</p>
 				
-					<form>
-					<label></label>
-					<input/>
-					</form>
+					<!-- AT Popup Beta 2015 BEGIN -->
+					<script type='text/javascript'>
+						(function () {
+							var _atpopq = window._atpopq || (window._atpopq = []);
+							window._atpopobj = {};
+							if (!_atpopq.loaded) {
+								var atpopjs = document.createElement('script');
+								atpopjs.type = 'text/javascript';
+								atpopjs.async = true;
+									atpopjs.src = '//cdn-media.web-view.net/popups/lib/v1/loader.min.js';
+								var s = document.getElementsByTagName('script')[0];
+								s.parentNode.insertBefore(atpopjs, s);
+								_atpopq.loaded = true;
+							}
+							_atpopq.push(['UserId', 'datjepuh8w']);
+							_atpopq.push(['PopupId', 'ja8u']);
+							_atpopq.push(['OnEventCallback', 'handleATPopupEvent']);
+						})();
+					</script>
+					<script type="text/javascript">
+						//Sample event handler function
+						function handleATPopupEvent(ev,args){
+							switch(ev){
+								case 'display':
+									//Do this when the popup is displayed
+									break;
+								case 'close':
+									//Do this when the popup gets closed by the user
+									break;
+								case 'submit':
+									//Do this when popup gets submitted and the user doesn't get redirected to a URL
+									break;
+							}
+						}
+					</script>
+					<!-- AT Popup Beta END -->
 				</div>
 			
 </section>
