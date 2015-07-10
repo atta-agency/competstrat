bfd7102eab6b054c245c0702379a351bca7c5555
koch <guillermo.jeanphilippe@gmail.com>
2015-07-10 11:37:59 +0200

    Structure include home

--- wp-content/themes/underscore_sass_v1/home.php
+++ wp-content/themes/underscore_sass_v1/home.php
@@ -1,12 +1,23 @@
 <?php 
 
 /*
-Template Name: Home page
+	Template Name: Home page
 */
 
 get_header();?>
 
+<?php include (TEMPLATEPATH . '/includes/home_motion.php'); ?>
 
+<?php include (TEMPLATEPATH . '/includes/home_description.php'); ?>
 
+<?php include (TEMPLATEPATH . '/includes/home_parallax.php'); ?>
+
+<?php include (TEMPLATEPATH . '/includes/home_map.php'); ?>
+
+<?php include (TEMPLATEPATH . '/includes/home_video.php'); ?>
+
+<?php include (TEMPLATEPATH . '/includes/home_club.php'); ?>
+
+<?php include (TEMPLATEPATH . '/includes/home_contact.php'); ?>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/underscore_sass_v1/includes/home_motion.php
@@ -0,0 +1,7 @@
+<div class="video">
+
+	<video id="video" autoplay muted poster="http://placehold.it/1400x400">
+    	<source src="<?php echo get_template_directory_uri(); ?>/assets/video/video.mp4" type="video/mp4">
+    </video>
+
+</div>
