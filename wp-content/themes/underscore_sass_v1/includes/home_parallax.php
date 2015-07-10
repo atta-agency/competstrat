cc18c215ee21ce5fa01eab94970a8b98654f0661
Sonia pham <phamsonia@gmail.com>
2015-07-10 16:54:07 +0200

    Partie Parallax

---
+++ wp-content/themes/underscore_sass_v1/includes/home_parallax.php
@@ -0,0 +1,33 @@
+<div id="parallax">
+	<section id="scrolling">
+			<figure id="" class="krill-piece">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/krill-piece.png" alt="Krill Tonic"/>
+			</figure> 
+
+		<img id="cloud1" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds.png" alt="" />
+		<img id="cloud2" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds4.png" alt=""/>
+		<img id="cloud3" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds2.png" alt=""/>
+		<img id="cloud4" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds3.png" alt=""/>
+		<img id="cloud5" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds4.png" alt=""/>
+		<img id="cloud6" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds3.png" alt=""/>
+		<img id="cloud7" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds3.png" alt=""/>
+		<img id="cloud8" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds.png" alt=""/>
+		<img id="cloud9" src="<?php echo get_template_directory_uri(); ?>/assets/img/clouds2.png" alt=""/>
+
+		<article id="textkrillpiece-left">
+			<h3>La boisson</h3>
+			<p>Each gas well requires an average of <em>400 tanker trucks</em> to carry water and supplies to and from the site. </p>
+		</article>
+	
+		<article id="textkrillpiece-right">
+			<h3>Les ingrédients </h3>
+			<p>Each gas well requires an average of <em>400 tanker trucks</em> to carry water and supplies to and from the site. </p>
+		</article>
+
+		<article id="textkrillpiece-left">
+			<h3>Blalalalalala</h3>
+			<p>Each gas well requires an average of <em>400 tanker trucks</em> to carry water and supplies to and from the site. </p>
+		</article>
+	</section>
+
+</div>
