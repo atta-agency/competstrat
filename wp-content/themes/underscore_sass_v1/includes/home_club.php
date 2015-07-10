6d7a3d327fae336998a48f2b596637447ee73878
koch <guillermo.jeanphilippe@gmail.com>
2015-07-10 16:41:00 +0200

    Ajout elements de structure

---
+++ wp-content/themes/underscore_sass_v1/includes/home_club.php
@@ -0,0 +1,30 @@
+<section class="home_club">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<form>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Nom</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Nom">
+					</div>
+
+					<div class="form-group">
+						<label for="exampleInputPassword1">Prenom</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Prenom">
+					</div>
+
+					<div class="checkbox">
+						<label>
+							<input type="checkbox"> Check me out
+						</label>
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
