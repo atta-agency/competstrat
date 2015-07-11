d53735ecda084bbb46a34dd4359fdd7eb62b2112
Sonia pham <phamsonia@gmail.com>
2015-07-11 15:46:58 +0200

    Map

---
+++ wp-content/themes/underscore_sass_v1/includes/home_map.php
@@ -0,0 +1,52 @@
+<div class="distribution-map">
+    <img src="http://i.imgur.com/M7aUkuS.png">
+    <!--In the original application, these points are injected with Javascript, but ideally, they'd be injected with a haml loop. Because I'm hardcoding content, I'm presenting this as prerendered HTML-->
+    <button class="map-point" style="top:15%;left:35%">
+        <div class="content">
+            <div class="centered-y">
+                <h2>A Place</h2>
+                <p>You can put plenty of details in here. In the original, I listed contact information and linked phone numbers and email addresses.</p>
+            </div>
+        </div>
+    </button>
+    <button class="map-point" style="top:35%;left:50%">
+        <div class="content">
+            <div class="centered-y">
+                <h2>Another Place</h2>
+                <p>Lorem ipsum something something</p>
+            </div>
+        </div>
+    </button>
+    <button class="map-point" style="top:76%;left:82.5%">
+        <div class="content">
+            <div class="centered-y">
+                <h2>Marauder Town</h2>
+                <p>I solemnly swear that I am up to no good</p>
+            </div>
+        </div>
+    </button>
+    <button class="map-point" style="top:45%;left:16%">
+        <div class="content">
+            <div class="centered-y">
+                <h2>Logan's Mum</h2>
+                <p>I&rsquo;m a marshmallow</p>
+            </div>
+        </div>
+    </button>
+    <button class="map-point" style="top:60%;left:53%">
+        <div class="content">
+            <div class="centered-y">
+                <h2>Toto</h2>
+                <p>I bless the rains</p>
+            </div>
+        </div>
+    </button>
+    <button class="map-point" style="top:25%;left:70%">
+        <div class="content">
+            <div class="centered-y">
+                <h2>With Love</h2>
+                <p>&mdash; 007</p>
+            </div>
+        </div>
+    </button>
+</div>
