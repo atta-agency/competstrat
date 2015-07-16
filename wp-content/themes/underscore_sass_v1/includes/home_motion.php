202d6788f01cbccb24c2f3d6fab7e3673f3145cb
Sonia pham <phamsonia@gmail.com>
2015-07-16 15:48:15 +0200

    video motion

--- wp-content/themes/underscore_sass_v1/includes/home_motion.php
+++ wp-content/themes/underscore_sass_v1/includes/home_motion.php
@@ -1,13 +1,14 @@
 <section class="video" id="video">
 
-	<?php /*<video id="video" autoplay muted poster="http://placehold.it/1400x400">
-    	<source src="<?php echo get_template_directory_uri(); ?>/assets/video/video.mp4" type="video/mp4">
-    </video> */ ?>
+
+ <video id="video" autoplay muted poster="http://placehold.it/1400x400">
+    	<source src="<?php echo get_template_directory_uri(); ?>/assets/video/video1.mp4" type="video/mp4">
+    </video> 
     
-    <div class="row">
+   <!-- <div class="row">
     	<img class="home_logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-home.png">
     </div>
-
+-->
     <div class="row">
     	 <div class="scroll_way revealOnScroll" data-animation="fadeIn anim-offset-sm">
 			<a class="scrollTo" href="#parallax"></a>
