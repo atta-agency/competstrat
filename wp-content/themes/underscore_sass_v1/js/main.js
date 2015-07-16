4a7e56d042f54a0b3feac9dd8b886332d07a970d
koch <guillermo.jeanphilippe@gmail.com>
2015-07-16 11:29:50 +0200

    Scroll header

--- wp-content/themes/underscore_sass_v1/js/main.js
+++ wp-content/themes/underscore_sass_v1/js/main.js
@@ -17,4 +17,14 @@ $(document).ready(function() {
 	  $(this).addClass('active');
 	});
 
+	$(window).scroll(function() {    
+	    var scroll = $(window).scrollTop();
+
+	     //>=, not <=
+	    if (scroll >= 500) {
+	        //clearHeader, not clearheader - caps H
+	        $(".header").addClass("visible-header");
+	    }
+	}); //missing );
+
 });
