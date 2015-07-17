08dbeb3fb99a632b008eb82f2b6ecbf3ce0a6c3d
koch <guillermo.jeanphilippe@gmail.com>
2015-07-17 19:38:25 +0200

    Add some sections

--- wp-content/themes/underscore_sass_v1/includes/home_art_de_vivre.php
+++ wp-content/themes/underscore_sass_v1/includes/home_art_de_vivre.php
@@ -1,9 +1,21 @@
 <section id="section_2" class="home_art">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-12">
-				<h1>ART DE VIVRE</h1>
+			<div class="col-sm-6">
+				<h2 class="special_title">
+				L'art de vivre<br/>
+				<strong>Krill Tonic</strong>	
+				</h2>
+				<p>
+					Krill Tonic vous permet de préserver la beauté et l’énergie, la fougue,
+					l’indéfectible ardeur de votre jeunesse, de ces années qui virent de belles pages s’écrire,
+					de beaux moments se vivre. Déguster Krill Tonic c’est s’arroger l’aptitude rare de pouvoir saisir le temps,
+					de dompter la clespydre, de maîtriser les situations les plus rudes, de subjuguer vos fréquentations,
+					mais c’est avant tout s’aimer soi même.
+				</p>
+				<p><strong>Krill Tonic est un art, un style de vie.</strong></p>
 			</div>
 		</div>
 	</div>
+	<img src="<?php echo get_template_directory_uri(); ?>/assets/img/georgie.png" class="portrait">
 </section>
--- wp-content/themes/underscore_sass_v1/includes/home_puissance.php
+++ wp-content/themes/underscore_sass_v1/includes/home_puissance.php
@@ -1,8 +1,20 @@
 <section id="section_3" class="home_puissance">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-12">
-				<h1>La puissance</h1>
+			<div class="col-sm-12 text-center">
+				<h2 class="special_title"><strong>La puissance</strong> du krill</h2>
+				<p>Découvrez le pouvoir fantasmagorique, l’irréfragable et intrinsèque aptitude à transmettre leur pouvoir des euphausiacés des eaux du grand nord enfin maîtrisés pour vous, pour cette fringante jeunesse qui sonnera le glas d’une indélicate vieillesse qu’il appert nécessaire de fuir.</p>
+				<div class="box_img">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/lac.jpg">
+					<div class="box_txt">
+						<p>Le Krill maintient également un équilibre entre les oméga 3 et les oméga 6 qui permet de préserver la santé des articulations et l’amplitude des mouvements. Enfin les nutriments vitaux qui contiennent les oméga 3 et les phospholipides aide à maintenir la santé du cerveau et l’acuité mentale. </p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2 text-center">
+				<p class="catch">Le Krill a été étudié méticuleusement pour prélever<br/> la dose parfaite afin de vous faire profiter de toute<br/> la magnificence de sa puissance.</p>
 			</div>
 		</div>
 	</div>
