288f077d7038c6534b5ff78363d2e8215bb06af2
koch <guillermo.jeanphilippe@gmail.com>
2015-07-18 15:30:02 +0200

    Fix skip link focus

--- wp-content/themes/underscore_sass_v1/functions.php
+++ wp-content/themes/underscore_sass_v1/functions.php
@@ -107,8 +107,6 @@ function underscore_sass_scripts() {
 
 	wp_enqueue_script( 'underscore_sass-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'underscore_sass-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
