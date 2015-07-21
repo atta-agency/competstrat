a73241f074b150b3ef6b12ef58bf69787ce6403c
Jean-Philippe Guillermo <gjp@ciblo.net>
2015-07-21 12:42:43 +0200

    Slider mob

--- wp-content/themes/underscore_sass_v1/includes/home_realisation.php
+++ wp-content/themes/underscore_sass_v1/includes/home_realisation.php
@@ -1,11 +1,60 @@
 <section id="section_5" class="home_realisation">
+	<div class="hidden-xs">
+		<div class="carousel-left">
+			<?php include (TEMPLATEPATH . '/includes/carousel/left.php'); ?>
+		</div>
 
-	<div class="carousel-left">
-		<?php include (TEMPLATEPATH . '/includes/carousel/left.php'); ?>
+		<div class="carousel-right">
+			<?php include (TEMPLATEPATH . '/includes/carousel/right.php'); ?>
+		</div>
 	</div>
+	<div class="visible-xs slider-mob">
+		<img class="first" src="<?php echo get_template_directory_uri(); ?>/assets/img/image-3.jpg">
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="caption-mob">
+				        <h2 class="special_title"><strong>Le Krill</strong></h2>
+				        <p class="lead">64°11'30.9"N 8°06'41.4"E</p>
+				        <p>Entre tradition et modernité, au coeur d’une immatérialité aujourd’hui maîtrisée, le krill est pêché et sélectionné scrupuleusement à la main, en vertu d’une artisanale méthode ancestrale par nos collaborateurs, des équipes motivées, dans les eaux glacées de l’arctique. </p>
+				    </div>	
+				</div>
+			</div>
+		</div>
+		
 
-	<div class="carousel-right">
-		<?php include (TEMPLATEPATH . '/includes/carousel/right.php'); ?>
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-2.jpg">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="caption-mob">
+				        <h2 class="special_title"><strong>La distillation</strong></h2>
+				        <p class="lead">56°26'00.7"N 3°25'51.7"W</p>
+				        <p>C’est au coeur des glaciers, au plus prêt de son habitat naturel et dans les meilleures conditions, que nous concoctons, que nous élaboraons, que nous vous offrons aujourd’hui  l’huile de Krill au sein de notre distillerie.</p>
+				    </div>	
+				</div>
+			</div>
+		</div>
+
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-1.jpg">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="caption-mob">
+				        <h2 class="special_title"><strong>La mise en fiole</strong></h2>
+				        <p class="lead">48° 51′ 24″ N 2°21′.07″ E</p>
+				        <p>Le précieux liquide est ensuite acheminé directement vers notre comptoir parisien idéalement situé au coeur du charmant quartier de l’Opéra, ce quartier au sein duquel vit tout autant la culture que la douceur de vivre.</p> 
+
+				        <p>C’est ici que nous élaborons la composition finale en ajoutant aux extraits de Krill l’ensemble des ingrédients naturels, cette nature qui alloue maintenant à Krill Tonic le coeur pulsionnel, battant, de ses bienfaits indéfectibles.</p>
+
+				    </div>	
+				</div>
+			</div>
+		</div>
+
 	</div>
 </section>
 
