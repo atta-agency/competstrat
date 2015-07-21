f087672be9ed51290c0303743f1569337b02705a
Jean-Philippe Guillermo <gjp@ciblo.net>
2015-07-21 12:09:00 +0200

    Full mobile support

--- wp-content/themes/underscore_sass_v1/includes/home_puissance.php
+++ wp-content/themes/underscore_sass_v1/includes/home_puissance.php
@@ -1,11 +1,19 @@
 <section id="section_3" class="home_puissance">
+	
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 text-center">
 				<h2 class="special_title"><strong>La puissance</strong> du krill</h2>
 				<p>Découvrez le pouvoir fantasmagorique, l’irréfragable et intrinsèque aptitude à transmettre leur pouvoir des euphausiacés des eaux du grand nord enfin maîtrisés pour vous, pour cette fringante jeunesse qui sonnera le glas d’une indélicate vieillesse qu’il appert nécessaire de fuir.</p>
+			</div>
+		</div>
+	</div>	
+	<img class="visible-xs box_img_mob" src="<?php echo get_template_directory_uri(); ?>/assets/img/lac-mob.jpg">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
 				<div class="box_img">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/lac.jpg">
+					<img class="hidden-xs" src="<?php echo get_template_directory_uri(); ?>/assets/img/lac.jpg">
 					<div class="box_txt">
 						<p>Le Krill maintient également un équilibre entre les oméga 3 et les oméga 6 qui permet de préserver la santé des articulations et l’amplitude des mouvements. Enfin les nutriments vitaux qui contiennent les oméga 3 et les phospholipides aide à maintenir la santé du cerveau et l’acuité mentale. </p>
 					</div>
